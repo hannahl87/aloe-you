7591daec69c3670bcf1d9cb17873b110ab46a0a9
Hannahvic <hannahvl1987@hotmail.com>
2021-12-29 13:53:39 +0000

    updating to display

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
