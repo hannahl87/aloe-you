89db74f6947e99af2caf7bf29a15859fddd39a10
Hannahvic <hannahvl1987@hotmail.com>
2021-12-30 09:23:32 +0000

    adding react-router-dom

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -4,16 +4,16 @@ import Directory from '../directory/directory.component';
 
 const Hero = () => (
   <div className='hero-container mx-8 md:col-span-3 md:mr-14'>
-    <div className='hero-title text-3xl'>
-      <h1>Boost Your Mood</h1>
+    <div className='hero-title text-4xl'>
+      <p>Boost Your Mood</p>
     </div>
     <div className='hero-subtitle text-base my-4'>
-      <h3>
+      <p>
         Plants have been shown to make you feel calm and relaxed. From air
         purifying, to easy care, we have you covered to bring your home to life.
         Shop our carefully grown, healthy and happy houseplants, delivered
         straight to your door!
-      </h3>
+      </p>
     </div>
     <Directory />
   </div>
