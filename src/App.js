3af7de00abc7c0498450396d4f79384a79d62c07
Hannahvic <hannahvl1987@hotmail.com>
2021-12-30 12:58:35 +0000

    collection page and collection items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 
 import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <div className='h-screen'>
         <Header />
-        <Homepage />
+        <Routes>
+          <Route path='/aloe-you/' element={<Homepage />} />
+          <Route path='/aloe-you/shop' element={<ShopPage />} />
+        </Routes>
       </div>
     </div>
   );
