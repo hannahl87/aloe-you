379bbd9d210edace615014f06d0aa546d231998b
Hannahvic <hannahvl1987@hotmail.com>
2022-01-07 17:29:34 +0000

    removing card hover scale

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './collection-item.styles.css';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item flex flex-col mb-4 cursor-pointer hover:scale-105 col-span-1'>
+  <div className='collection-item flex flex-col mb-4 cursor-pointer col-span-1'>
     <div className='top-card flex justify-center rounded-t-xl'>
       <img
         src={imageUrl}
