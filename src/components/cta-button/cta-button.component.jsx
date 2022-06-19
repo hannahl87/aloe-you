9f2124469b4de46990af0d74b671abad9fc567d4
Hannah <hannahlynn.dev@gmail.com>
2022-06-19 13:24:01 +0100

    use useNavigate hook instead of link on cta btn

--- src/components/cta-button/cta-button.component.jsx
+++ src/components/cta-button/cta-button.component.jsx
@@ -1,15 +1,20 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './cta-button.styles.scss';
 
-const CtaButton = ({ text, link }) => (
-  <div className='cta-container'>
-    <Link
-      to={link}
-      className='cta-btn rounded-3xl px-5 py-2 mt-2 hover:scale-105'
-    >
-      {text}
-    </Link>
-  </div>
-);
+const CtaButton = ({ text, link }) => {
+  const navigate = useNavigate();
+  const goToLink = () => navigate(link);
+
+  return (
+    <div className='cta-container'>
+      <button
+        onClick={goToLink}
+        className='cta-btn rounded-3xl px-5 py-2 mt-2 hover:scale-105'
+      >
+        {text}
+      </button>
+    </div>
+  );
+};
 
 export default CtaButton;
