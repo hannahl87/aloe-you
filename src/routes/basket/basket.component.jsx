b39d0380f9055f1bbfde2c0b8f3b9fb078c77252
Hannah <hannahlynn.dev@gmail.com>
2022-06-20 20:21:29 +0100

    close dropdown when on basket page

--- src/routes/basket/basket.component.jsx
+++ src/routes/basket/basket.component.jsx
@@ -1,19 +1,38 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import BasketCard from '../../components/basket-card/basket-card.component';
+import CtaButton from '../../components/cta-button/cta-button.component';
 import { BasketContext } from '../../contexts/basket.context';
 import './basket.component.scss';
 
 const Basket = () => {
-  const { basketItems, basketTotal } = useContext(BasketContext);
+  const { basketItems, basketTotal, setIsBasketOpen } =
+    useContext(BasketContext);
+
+  useEffect(() => {
+    setIsBasketOpen(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className='basket-page mt-20 mx-14'>
-      <div className='items'>
-        {basketItems.map((item) => {
-          return <BasketCard key={item.id} item={item} />;
-        })}
-      </div>
-      <div className='total flex justify-end'>Total: £{basketTotal}</div>
+      {basketItems.length ? (
+        <>
+          <div className='items'>
+            {basketItems.map((item) => {
+              return <BasketCard key={item.id} item={item} />;
+            })}
+          </div>
+          <div className='basket-total flex justify-center'>
+            <h2 className='total self-center mr-4'>Total: £{basketTotal}</h2>
+            <CtaButton link={'/checkout'} text='Go to Checkout'></CtaButton>
+          </div>
+        </>
+      ) : (
+        <div className='empty-basket'>
+          {' '}
+          You don't have any products in your basket{' '}
+        </div>
+      )}
     </div>
   );
 };
