1beb0fc749b2d0202be6c91954caebf851296a4a
Hannah <hannahlynn.dev@gmail.com>
2022-06-25 16:37:29 +0100

    fix double quotes on donenv and stripe in netlify functions

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,28 +1,28 @@
-require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
-console.log('event :', event);
-try {
-  const { amount } = JSON.parse(event.body);
+  console.log("event :", event);
+  try {
+    const { amount } = JSON.parse(event.body);
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount,
-    currency: 'gbp',
-    payment_method_types: ['card'],
-  });
-  console.log('paymentIntent :', paymentIntent);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "gbp",
+      payment_method_types: ["card"],
+    });
+    console.log("paymentIntent :", paymentIntent);
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ paymentIntent }),
-  };
-} catch (error) {
-  console.log(error);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ paymentIntent }),
+    };
+  } catch (error) {
+    console.log(error);
 
-  return {
-    statusCode: 400,
-    body: JSON.stringify({ error: error }),
-  };
-}
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: error }),
+    };
+  }
 };
