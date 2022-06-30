9ef9dab59eaa1385cddc3be99d9fcbab2928b276
Hannah <hannahlynn.dev@gmail.com>
2022-06-30 07:21:16 +0100

    display prompt to login at checkout

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,7 +1,19 @@
+import { useContext } from 'react';
 import PaymentForm from '../../components/payment-form/payment-form.component';
+import { UserContext } from '../../contexts/user.context';
 import './checkout.styles.scss';
 
 const Checkout = () => {
+  const { currentUser } = useContext(UserContext);
+
+  if (!currentUser) {
+    return (
+      <div className='checkout-page mt-20 mx-14 flex flex-col items-center'>
+        You must be logged in to checkout
+      </div>
+    );
+  }
+
   return (
     <div className='checkout-page mt-20 mx-14 flex flex-col items-center'>
       <div className='payment-form-container forms flex flex-col py-6 px-8 rounded-xl mt-6 h-44 w-96'>
