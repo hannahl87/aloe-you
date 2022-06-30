848cb7cd6414dfb0002e6a716e4d45912399b503
Hannah <hannahlynn.dev@gmail.com>
2022-06-30 17:46:24 +0100

    fix: no orders div on my account

--- src/components/orders-table/orders-table.component.jsx
+++ src/components/orders-table/orders-table.component.jsx
@@ -13,28 +13,28 @@ const OrdersTable = () => {
   return (
     <div className='orders-table mt-6 w-full'>
       <h3 className='text-l font-bold flex justify-center my-6'>My Orders</h3>
-      <table className='w-full border-1 border-cyan-300'>
-        <thead>
-          <tr>
-            <th>Order Date</th>
-            <th>Order Total</th>
-            <th>Order Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {customerOrders.length ? (
-            customerOrders.map((order) => (
+      {customerOrders.length ? (
+        <table className='w-full border-1 border-cyan-300'>
+          <thead>
+            <tr>
+              <th>Order Date</th>
+              <th>Order Total</th>
+              <th>Order Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {customerOrders.map((order) => (
               <tr key={order.created}>
                 <td>{convertDate(order.created)}</td>
                 <td>£{order.amount}</td>
                 <td>{order.status}</td>
               </tr>
-            ))
-          ) : (
-            <tr>You don't have any orders yet</tr>
-          )}
-        </tbody>
-      </table>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className='no-orders'>You don't have any orders yet</div>
+      )}
     </div>
   );
 };
