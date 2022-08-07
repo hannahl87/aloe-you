3f1f0f71d488438faaf9bac38ff2f59a0e60ba58
Hannah <hannahlynn.dev@gmail.com>
2022-08-07 15:29:16 +0100

    fix: link to all shop items on mobile

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,6 +4,7 @@ import './directory.styles.scss';
 import monstera from '../../assets/images/monstera.webp';
 import pothos from '../../assets/images/scindapusus.webp';
 import rubber from '../../assets/images/rubber-plant.webp';
+import CtaButton from '../cta-button/cta-button.component';
 
 class Directory extends React.Component {
   constructor() {
@@ -36,6 +37,9 @@ class Directory extends React.Component {
         {this.state.categories.map(({ id, title, img }) => (
           <ShopCard key={id} type={title} img={img} />
         ))}
+        <div className='mobile-shop-cta m-8 inline md:hidden'>
+          <CtaButton link='shop' text='Shop all' />
+        </div>
       </div>
     );
   }
