64193d6ee1e263a0fcbaca86c888012ffa485cb9
Hannah <hannahlynn.dev@gmail.com>
2022-08-08 14:12:01 +0100

    fix: margin y on login/signup

--- src/routes/login-signup/login-signup.component.jsx
+++ src/routes/login-signup/login-signup.component.jsx
@@ -6,7 +6,7 @@ import './login-signup.styles.scss';
 const LoginSignUpPage = () => (
   <div className='flex flex-col items-center justify-between m-14'>
     <h2 className='text-xl font-bold'>Aloe You!</h2>
-    <div className='forms flex flex-col py-6 px-8 rounded-xl md:flex-row md:space-x-20 mt-6'>
+    <div className='forms flex flex-col py-6 px-8 rounded-xl md:flex-row md:space-x-20 my-6'>
       <Login />
       <SignUp />
     </div>
